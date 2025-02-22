f818a6b7b577909fb55f8c59607ac8af99627f16
komalkrishn <komal.kavula999@gmail.com>
2025-02-22 15:41:12 -0500

    Feat: add preloader and particles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import React, { useState, useEffect } from "react";
 import Home from "./components/Home/Home";
+import Preloader from "../src/components/Preloader";
 import {
   BrowserRouter as Router,
   Route,
@@ -6,14 +8,29 @@ import {
   Navigate
 } from "react-router-dom";
 import "./App.css";
+import "./style.css";
+import Footer from "./components/Footer";
+import NavBar from "./components/Navbar";
 
 function App() {
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <Router>
-      <div>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+      <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <Footer />
        </div>
     </Router>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by Komal krishna</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} KK</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/komalkrishn"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://x.com/Komal11055700"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiOutlineTwitter />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/komal-krishna-kavula/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
